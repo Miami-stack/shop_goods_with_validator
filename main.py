ee2cce06e50b89cbe77f79e8239e4ca5bfec7374
Miami-stack <muratajmedov@gmail.com>
2020-12-15 17:56:16 +0500

    ssdsd

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import json
+import jsonschema
+
+
+with open("goods.schema.json") as e:
+    data = json.load(e)
+
+for i in data['examples']:
+    print(i)
+    print(i['name'], i['package_params']['width'], i['package_params']['height'])
+    for j in i['location_and_quantity']:
+        print(j)
 
